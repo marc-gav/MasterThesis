583db34daa7510ea94faa4ed081f3aa9b178032b
Marc Gavilan <marcgavilangil@gmail.com>
2022-11-17 12:18:48 +0100

    feat: Improve cluster_embeddings.py

--- cluster_embeddings.py
+++ cluster_embeddings.py
@@ -7,21 +7,9 @@ import pandas as pd
 import yaml
 from sklearn.decomposition import PCA
 from tqdm import tqdm
-
+import mplcursors
 
 def parse_arguments(parser: argparse.ArgumentParser) -> argparse.Namespace:
-    parser.add_argument(
-        "--target_word",
-        type=str,
-        required=True,
-        help="Target word to cluster",
-    )
-    parser.add_argument(
-        "--embedding_file",
-        type=str,
-        required=False,
-        help="Path to the csv file containing the embeddings and the context sentence",
-    )
     parser.add_argument(
         "--clustering_method",
         type=str,
@@ -29,34 +17,48 @@ def parse_arguments(parser: argparse.ArgumentParser) -> argparse.Namespace:
         default="DPBGMM",
         help="Clustering method to use",
     )
+
     parser.add_argument(
-        "--clustering_method_settings_file",
+        "--target_word",
+        type=str,
+        required=True,
+        help="Target word to cluster",
+    )
+
+    parser.add_argument(
+        "--embedding_file",
         type=str,
         required=False,
-        default="clustering_settings.yml",
-        help="Path to the yaml file containing the settings for the clustering methods",
+        help="Path to the csv file containing the embeddings and the context sentence",
     )
+
     parser.add_argument(
         "--save_file",
         type=bool,
         required=False,
-        default=True,
+        default=False,
         help="Whether to save the cluster labels",
     )
+
     parser.add_argument(
         "--save_path",
         type=str,
         required=False,
-        default=f"data/{parser.parse_args().target_word}_embeddings_with_cluster_labels.csv",
         help="Path to save the cluster labels",
     )
 
-    # Summarize the arguments
-    print("Processing with arguments:")
-    for arg in vars(parser.parse_args()):
-        print(f"\t{arg}: {getattr(parser.parse_args(), arg)}")
+    target = parser.parse_args().target_word
 
-    return parser.parse_args()
+    # Convert parser.parse_args() to a dictionary
+    arguments = vars(parser.parse_args())
+
+    if not arguments['save_path']:
+        arguments['save_path'] = f"data/{target}_embeddings_clustered.csv"
+    
+    if not arguments['embedding_file']:
+        arguments['embedding_file'] = f"data/{target}_embeddings.csv"
+
+    return arguments
 
 
 def cluster_embeddings(
@@ -114,15 +116,46 @@ def cluster_embeddings(
     return labels
 
 
+def plot_embeddings(
+    df_clustered: pd.DataFrame,
+):
+    _, ax = plt.subplots()
+    ax.scatter(
+        pca_embeddings[:, 0],
+        pca_embeddings[:, 1],
+        alpha=0.9,
+        c=labels,
+        cmap="tab10",
+    )
+
+    mplcursors.cursor(hover=True).connect(
+        "add", lambda sel: sel.annotation.set_text(df_clustered.iloc[sel.index]["sentence"])
+    )
+
+
+    ax.set_title("Bert embeddings of word 'plant' in sentence")
+    ax.set_xlabel("PCA component 1")
+    ax.set_ylabel("PCA component 2")
+    ax.set_aspect("equal")
+    ax.set_facecolor("white")
+    ax.grid(False)
+    ax.set_frame_on(False)
+    ax.set_axisbelow(False)
+    ax.set_aspect("equal")
+
+    plt.tight_layout()
+    plt.show()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Cluster BERT embeddings and plot them in a 2D space using PCA."
+        description="""Cluster BERT embeddings and plot them in a 2D space using PCA""",
+        epilog="""By default it will look for the embeddings in data/{target_word}_embeddings.csv"""
     )
     args = parse_arguments(parser)
-    target_word = parser.parse_args().target_word
-    embeddings_file = parser.parse_args().embedding_file
-    clustering_method = parser.parse_args().clustering_method
-    clustering_method_settings_file = args.clustering_method_settings_file
+    target_word = args['target_word']
+    embeddings_file = args['embedding_file']
+    clustering_method = args['clustering_method']
+    clustering_method_settings_file = 'clustering_settings.yml'
     df_embeddings = pd.read_csv(embeddings_file, sep="|")
     df_embeddings["target_word_vector"] = df_embeddings[
         "target_word_vector"
@@ -149,38 +182,19 @@ if __name__ == "__main__":
         clustering_method,
         clustering_method_settings_file,
     )
-
-    # save labels to csv file for further analysis
     df_clustered = df_embeddings.copy()
     df_clustered["cluster_label"] = labels
     df_clustered["clustering_method"] = clustering_method
     df_clustered["target_word"] = target_word
     # drop target_word_vector column
-    df_clustered.drop(columns=["target_word_vector"], inplace=True)
-    df_clustered.to_csv(
-        f"{args.save_path}",
-        sep="|",
-        index=False,
-    )
-    print(f"Cluster labels saved to {args.save_path}")
 
-    fig, ax = plt.subplots()
-    ax.scatter(
-        pca_embeddings[:, 0],
-        pca_embeddings[:, 1],
-        alpha=0.9,
-        c=labels,
-        cmap="tab10",
-    )
-    ax.set_title("Bert embeddings of word 'plant' in sentence")
-    ax.set_xlabel("PCA component 1")
-    ax.set_ylabel("PCA component 2")
-    ax.set_aspect("equal")
-    ax.set_facecolor("white")
-    ax.grid(False)
-    ax.set_frame_on(False)
-    ax.set_axisbelow(False)
-    ax.set_aspect("equal")
+    if args['save_file']:
+        df_clustered.drop(columns=["target_word_vector"], inplace=True)
+        df_clustered.to_csv(
+            f"{args['save_path']}",
+            sep="|",
+            index=False,
+        )
+        print(f"Cluster labels saved to {args['save_path']}")
 
-    plt.tight_layout()
-    plt.show()
+    plot_embeddings(df_clustered)
