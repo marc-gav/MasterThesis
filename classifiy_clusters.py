f3f866b435b4169a267aaf931a32ac9b5b4d5837
Marc Gavilan <marcgavilangil@gmail.com>
2022-11-17 18:06:24 +0100

    feat: Setup wandb + pytorch lightning

--- classifiy_clusters.py
+++ classifiy_clusters.py
@@ -1,24 +1,31 @@
 import pandas as pd
+from torch import optim, nn, utils, Tensor
 import torch
 from torch.utils.data import DataLoader, Dataset
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+import wandb
+import yaml
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
+from wandb.sdk.wandb_config import Config
 
 class ClusteredWordsDataset(Dataset):
     def __init__(self, data_dir: str):
         df = pd.read_csv(data_dir, sep="|", index_col=False)
         self.num_classes = len(df["cluster_label"].unique())
         self.salience_score = df["salience score"]
-        data = df["word"]
-        # Get word dict from data
-        self.word_dict = {word: i for i, word in enumerate(data)}
-        # One hot encodding representation of data using word_dict
-        self.vocab_size = len(self.word_dict)
-        self.data = torch.zeros(len(data), self.vocab_size)
+        words = df["word"]
 
-        for i, word in enumerate(data):
-            self.data[i][self.word_dict[word]] = 1
+        # Create a dict where every new word is mapped to an increasing index
+        word_to_idx: dict = {}
+        for word in words:
+            if word not in word_to_idx:
+                word_to_idx[word] = len(word_to_idx)
+        vocab_size = len(word_to_idx)
+        one_hot_matrix = torch.zeros((len(words), vocab_size))
+        for i, word in enumerate(words):
+            one_hot_matrix[i, word_to_idx[word]] = 1
+
+        self.data = one_hot_matrix
         self.labels = torch.tensor(
             df["cluster_label"].to_numpy(), dtype=torch.long
         )
@@ -30,119 +37,71 @@ class ClusteredWordsDataset(Dataset):
         return self.data[idx], self.labels[idx]
 
     def get_num_classes(self):
+        """Returns the number of classes in the dataset (clusters)"""
         return self.num_classes
 
     def get_salience_score(self):
+        """Returns the salience score of the dataset"""
         return self.salience_score
-
+    
     def get_vocab_size(self):
-        return self.vocab_size
-
-    def get_word_dict(self):
-        return self.word_dict
+        """Returns the size of the vocabulary"""
+        return self.data.shape[1]
 
 
-class ClusteredWordsClassifier(torch.nn.Module):
-    def __init__(
-        self, num_embeddings: int, embedding_dim: int, num_classes: int
-    ):
+class ProbingClassifier(pl.LightningModule):
+    def __init__(self, hyperparams: Config):
         super().__init__()
-        self.embedding = torch.nn.Embedding(num_embeddings, embedding_dim)
-        self.linear = torch.nn.Linear(embedding_dim, num_classes)
-
+        self.hyperparams = hyperparams
+        self.fc1 = nn.Linear(self.hyperparams.vocab_size, 100)
+        self.fc2 = nn.Linear(100, self.hyperparams.num_classes)
+    
     def forward(self, x):
-        x = self.embedding(x)
-        x = torch.mean(x, dim=1)
-        x = self.linear(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
         return x
+    
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = nn.functional.cross_entropy(y_hat, y)
+        self.log("train_loss", loss)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = nn.functional.cross_entropy(y_hat, y)
+        self.log("val_loss", loss)
+        return loss
+    
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=self.hyperparams.lr)
 
+PROJECT_NAME = 'Probing classifier'
+DATASET = ClusteredWordsDataset("data/plant_attention_scores.csv")
 
-def train(
-    model,
-    train_loader,
-    optimizer,
-    criterion,
-    num_epochs,
-    device,
-    save_path,
-    save_every,
-):
-    model.train()
-    for epoch in range(num_epochs):
-        for i, (data, labels) in enumerate(train_loader):
-            data = data
-            labels = labels.to(device)
-            optimizer.zero_grad()
-            outputs = model(data)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            if (i + 1) % 100 == 0:
-                print(
-                    f"Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{len(train_loader)}], Loss: {loss.item():.4f}"
-                )
-        if (epoch + 1) % save_every == 0:
-            torch.save(model.state_dict(), f"{save_path}/model_{epoch}.pt")
-    torch.save(model.state_dict(), f"{save_path}/model_final.pt")
+def train_experiment():
+    """Trains the model and logs the results to wandb"""
 
+    run = wandb.init(project=PROJECT_NAME)
+    wandb_logger = WandbLogger()
+    config = run.config
+    config.update({
+        "vocab_size": DATASET.get_vocab_size(),
+        "num_classes": DATASET.get_num_classes(),
+    })
+    probing_clas = ProbingClassifier(config)
+    train_loader = DataLoader(DATASET, batch_size=config.batch_size, shuffle=True)
+    val_loader = DataLoader(DATASET, batch_size=config.batch_size, shuffle=False)
+    trainer = pl.Trainer(limit_train_batches=100, max_epochs=10, logger=wandb_logger)
+    trainer.fit(model=probing_clas, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
-def test(model, test_loader, device):
-    model.eval()
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        for data, labels in test_loader:
-            data = data.to(device)
-            labels = labels.to(device)
-            outputs = model(data)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-        print(
-            f"Accuracy of the model on the test set: {100 * correct / total:.2f}%"
-        )
-
-
-def main():
-    data_dir = "data/plant_top_attention_words.csv"
-    save_path = "models"
-    num_epochs = 10
-    batch_size = 64
-    learning_rate = 0.001
-    save_every = 2
-    dataset = ClusteredWordsDataset(data_dir)
-    num_classes = dataset.get_num_classes()
-    salience_score = dataset.get_salience_score()
-    train_size = int(0.8 * len(dataset))
-    test_size = len(dataset) - train_size
-    train_dataset, test_dataset = torch.utils.data.random_split(
-        dataset, [train_size, test_size]
-    )
-    train_loader = DataLoader(
-        dataset=train_dataset, batch_size=batch_size, shuffle=True
-    )
-    test_loader = DataLoader(
-        dataset=test_dataset, batch_size=batch_size, shuffle=False
-    )
-    model = ClusteredWordsClassifier(
-        num_embeddings=len(dataset),
-        embedding_dim=dataset.get_vocab_size(),
-        num_classes=num_classes,
-    ).to(device)
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    train(
-        model,
-        train_loader,
-        optimizer,
-        criterion,
-        num_epochs,
-        device,
-        save_path,
-        save_every,
-    )
-    test(model, test_loader, device)
-
+    wandb.finish()
 
 if __name__ == "__main__":
-    main()
+    with open('experiments/sweep.yaml') as f:
+        sweep_config = yaml.load(f, Loader=yaml.FullLoader)
+
+    sweep_id = wandb.sweep(sweep_config, project=PROJECT_NAME)
+    wandb.agent(sweep_id, function=train_experiment)
